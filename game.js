314a2e8de824d5adeace3edfdbac6672b84bdc2f
CoutinhoGustav <gustavocoutinho32@gmail.com>
2026-01-09 17:40:57 -0300

    atualização

--- game.js
+++ game.js
@@ -270,7 +270,9 @@ backBtn.onclick = () => {
 const avatares = [
   'https://i.pravatar.cc/150?img=11',
   'https://i.pravatar.cc/150?img=12',
-  'https://i.pravatar.cc/150?img=13'
+  'https://i.pravatar.cc/150?img=13',
+  'https://i.pravatar.cc/150?img=14',
+  'https://i.pravatar.cc/150?img=15'
 ];
 
 let avatarTemp = null;
